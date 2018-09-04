96ca8b7e8f16438101a2391e32140b9363237b2c
ChaytonL <chaytonlofgren@gmail.com>
2018-09-04 10:17:07 -0500

    Temp Conversion Finished

--- Tempature Conversion/Tempature Conversion/Source.cpp
+++ Tempature Conversion/Tempature Conversion/Source.cpp
@@ -24,11 +24,13 @@ int main()
 	cTemp12 = (fTemp12 - 32) * 5 / 9;
 	cTemp5 = (fTemp5 - 32) * 5 / 9;
 
-	cout << " The tempature at 8am is " << fTemp8 << " Fahrenheit and " << cTemp8 << "Celsius";
+	cout << " The tempature at 8am is " << fTemp8 << " Fahrenheit and " << cTemp8 << " Celsius" << endl;
 
-	cout << " The tempature at 8am is " << fTemp8 << " Fahrenheit and " << cTemp8 << "Celsius";
+	cout << " The tempature at 12pm is " << fTemp12 << " Fahrenheit and " << cTemp12 << " Celsius" << endl;
 
-	cout << " The tempature at 8am is " << fTemp8 << " Fahrenheit and " << cTemp8 << "Celsius";
+	cout << " The tempature at 5pm is " << fTemp5 << " Fahrenheit and " << cTemp5 << " Celsius" << endl;
 
+	system("pause");
+	return 0;
 
 }

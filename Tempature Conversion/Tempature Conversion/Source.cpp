9f369ca537a6112cba5e124bf2f711e96db33e56
ChaytonL <chaytonlofgren@gmail.com>
2018-09-28 11:19:22 -0500

    1

--- Tempature Conversion/Tempature Conversion/Source.cpp
+++ Tempature Conversion/Tempature Conversion/Source.cpp
@@ -11,27 +11,19 @@ int tempConversion(double fahren) {
 
 int main()
 {
-	int fTemp8;
-	int fTemp12;
-	int fTemp5;
-	int cTemp;
+	
+	double z;
+	double tempArray[5] = { 0.0 };
+	// for loop
+	for (int i = 0; i < size(tempArray); i++) {
+		cout << " Enter a tempature >> ";
+		cin >> tempArray[i];
+		z = tempConversion(tempArray[i]);
+		cout << " The temp conversion is " << z << endl;
+	}
 
-	cout << " What was the tempature in Fahrenheit at 8am ";
-	cin >> fTemp8;
-
-	cout << " What was the tempature in Fahrenheit at 12pm ";
-	cin >> fTemp12;
-
-	cout << " What was the tempature in Fahrenheit at 5pm ";
-	cin >> fTemp5;
-
-	cTemp = tempConversion(fTemp8);
-	cout << " The tempature at 8am is " << fTemp8 << " Fahrenheit and " << cTemp << " Celsius" << endl;
-	cTemp = tempConversion(fTemp12);
-	cout << " The tempature at 12pm is " << fTemp12 << " Fahrenheit and " << cTemp << " Celsius" << endl;
-	cTemp = tempConversion(fTemp5);
-	cout << " The tempature at 5pm is " << fTemp5 << " Fahrenheit and " << cTemp << " Celsius" << endl;
-
+	
+	
 	system("pause");
 	return 0;
 
